1fa4b04dcfad13bb3eab4e2fcf189c5d8ab31611
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2024-12-20 17:30:13 +0530

    4 pillers of DOM Basic

---
+++ script.js
@@ -0,0 +1,84 @@
+// // FOUR Pillars of DOM--->
+
+// // 1. Selection of an Elements
+// // 2. Changing HTML
+// // 3. Changing CSS
+// // 4.EVent Listener
+// //!--------------------------------------------------------------------
+
+
+//  // 1. Selection of an Elements--------->>>
+
+
+//  var a = document.querySelector("h1")
+//  console.log(a);
+
+
+
+//  // 2. Changing HTML
+//  a.innerHTML="Changed HTML"
+
+
+
+//  // 3. Changing CSS
+//  a.style.color="red"
+//  a.style.backgroundColor="black"
+//  a.style.cursor="pointer"
+
+
+
+//  // 4.Event Listener
+//  a.addEventListener("click",function(){
+//     a.innerHTML="Badal gya hu mai teri Ex ki tarh"
+//     a.style.color="yellow"
+//     a.style.backgroundColor="black"
+
+//     alert("4 Pillers Distroyed")
+//  })
+
+// ---------------------------------------------------
+
+
+
+var bulb=document.querySelector("#bulb")
+var btn=document.querySelector("button")
+
+var count=0;
+btn.addEventListener("click",function(){
+    if(count==0){
+        bulb.style.backgroundColor="yellow"; 
+        bulb.style.boxShadow="50px 50px 100px 100px #e09d4b"
+        bulb.style.border="0px";
+        btn.innerHTML="Off"
+        btn.style.backgroundColor="red"
+        console.log("Bulb jal gya re ");
+        count=1;
+    } 
+    
+   
+    else{
+        bulb.style.backgroundColor="transparent";
+        bulb.style.boxShadow="none"
+        console.log("Bulb Bujh gya re Baba ");
+        btn.innerHTML="on"         
+        btn.style.backgroundColor="green"
+        count=0;
+        
+        
+
+    }
+    
+})
+
+// ---------------------------------------------------2
+
+var h=document.querySelector("h1")
+console.log(h,"Will Print 1st H1 ");
+
+// what if i want to select all h1....?
+var hh=document.querySelectorAll("h1")
+console.log(hh,"Will print all H1,but in the form of Node list slightly similar to  arrray)");
+
+hh.forEach(function(elem){
+    console.log(elem);//Will print all h1 individually
+})
